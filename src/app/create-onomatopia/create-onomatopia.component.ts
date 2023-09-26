9cb172aa7c25d903ec5529506a3126c0ede30c36
AurelOconnell <aurelienoconnell@gmail.com>
2023-09-26 16:13:15 +0200

    quest end

---
+++ src/app/create-onomatopia/create-onomatopia.component.ts
@@ -0,0 +1,19 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-create-onomatopia',
+  templateUrl: './create-onomatopia.component.html',
+  styleUrls: ['./create-onomatopia.component.css']
+})
+export class CreateOnomatopiaComponent {
+  newOnomatopia: string = '';
+  @Output() sendOnomatopiaToParent = new EventEmitter<string>();
+
+  createOnomatopia() {
+    if (this.newOnomatopia.trim() !== '') {
+      this.sendOnomatopiaToParent.emit(this.newOnomatopia);
+      this.newOnomatopia = '';
+    }
+  }
+
+}
